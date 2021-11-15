5092550440ee47b2c1692074e14bdb765e393f8b
zlatanlgd <zlatanlgd@outlook.com>
2021-11-15 23:01:12 +0800

    update

---
+++ code_round1/src/main/java/cn/zlatanlgd/programmercarl/150.逆波兰表达式求值.java
@@ -0,0 +1,55 @@
+import java.util.ArrayDeque;
+
+/*
+ * @lc app=leetcode.cn id=150 lang=java
+ *
+ * [150] 逆波兰表达式求值
+ */
+
+// @lc code=start
+class Solution {
+    public int evalRPN(String[] tokens) {
+        // ArrayDeque<Integer> stack = new ArrayDeque<>();
+        // for (String token : tokens) {
+        //     char c = token.charAt(0);
+        //     if (c == '+' || c == '-' || c == '*' || c == '/') {
+        //         int num1 = stack.pop();
+        //         int num2 = stack.pop();
+        //         if (c == '+')
+        //             stack.push(num1 + num2);
+        //         if (c == '-')
+        //             stack.push(num2 - num1);
+        //         if (c == '*')
+        //             stack.push(num2 * num1);
+        //         if (c == '/')
+        //             stack.push(num2 / num1);
+        //     } else {
+        //         stack.push(StoI(c));
+        //     }
+        // }
+        // return stack.pop();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String t : tokens) {
+            switch (t){
+                case "+": 
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    int num = stack.pop();
+                    stack.push(stack.pop() - num);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int num2 = stack.pop();
+                    stack.push(stack.pop() / num2);
+                    break;
+                default:
+                    stack.push(Integer.valueOf(t));
+            }x
+        }
+        return stack.pop();
+    }
+}
+// @lc code=end
